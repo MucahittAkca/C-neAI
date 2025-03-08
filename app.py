abfaf8c9de3d8501c48252ec50e7ca10b374bb1b
Mücahit Akca <Mucoakca@users.noreply.huggingface.co>
2025-03-08 15:16:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ final_answer = FinalAnswerTool()
 model = HfApiModel(
 max_tokens=2096,
 temperature=0.5,
-model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
+model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud',# it is possible that this model may be overloaded
 custom_role_conversions=None,
 )
 
